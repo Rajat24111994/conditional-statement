18642c91bb63bbe764727052599f3f93617ffd6d
Rajat24111994 <rajatmohangpt3@gmail.com>
2022-10-19 14:02:07 +0530

    second commit

---
+++ problem4.js
@@ -0,0 +1,16 @@
+let x = "Report Card"
+
+let a = "Name-Rajat Mohan";
+let b = "school name- Adarsh school";
+let c = "Grade B";
+let d = "Section A";
+let e = "Roll No 35";
+let f = "Math-95, English-84, Science 91";
+
+console.log(x);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
